b98b6d17875932eee12bdf9bbaea0223e5f904a2
谢军 <sgvenday@outlook.com>
2019-08-26 21:01:02 +0800

    要注意的点

--- spring-orm/jpa-complex/src/main/java/demo/spring/jpacomplex/dao/BaseDao.java
+++ spring-orm/jpa-complex/src/main/java/demo/spring/jpacomplex/dao/BaseDao.java
@@ -7,5 +7,14 @@ import java.util.List;
 
 @NoRepositoryBean
 public interface BaseDao<T, Long> extends PagingAndSortingRepository<T, Long> {
+    /**
+     * 注意：findTop3后面加了一个By
+     * findTop3后面是OrderBy，所以findTop3后面没有过滤条件也需要加By
+     *
+     * 第一个By用作分隔符以指示实际标准的开始
+     *
+     * 参考文档：https://docs.spring.io/spring-data/data-commons/docs/1.11.0.RELEASE/reference/html/#repositories.query-methods.details
+     * @return
+     */
     List<T> findTop3ByOrderByUpdateTimeDescIdAsc();
 }
--- spring-orm/jpa-complex/src/main/java/demo/spring/jpacomplex/dao/OrderDao.java
+++ spring-orm/jpa-complex/src/main/java/demo/spring/jpacomplex/dao/OrderDao.java
@@ -7,5 +7,18 @@ import java.util.List;
 
 public interface OrderDao extends BaseDao<Order, Long>{
     List<Order> findByCustomerOrderById(String customer);
+
+    /**
+     * 注意：Items_Name
+     * findByItemsName也能进去查找到Items的name属性，但可能Order也有ItemsName属性呢？那么findByItemsName就会按照ItemsName来查找了
+     * 所以为了避免混淆，在Items后面加下划线
+     *
+     * 属性表达式只能引用被管实体的直接属性
+     * “_”用来解决歧义
+     *
+     * 参考文档：https://docs.spring.io/spring-data/data-commons/docs/1.11.0.RELEASE/reference/html/#repositories.query-methods.query-property-expressions
+     * @param name
+     * @return
+     */
     List<Order> findByItems_Name(String name);
 }
