580faf9760a23256b7d58ffcfb58b6af9a978eab
谢军 <sgvenday@outlook.com>
2019-08-26 18:25:45 +0800

    update

--- spring-jdbc/transaction-propagation/src/test/java/demo/spring/transactionpropagation/TransactionPropagationApplicationTests.java
+++ spring-jdbc/transaction-propagation/src/test/java/demo/spring/transactionpropagation/TransactionPropagationApplicationTests.java
@@ -1,4 +1,4 @@
-package demo.spring.nodependency;
+package demo.spring.transactionpropagation;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NoDependencyApplicationTests {
+public class TransactionPropagationApplicationTests {
 
 	@Test
 	public void contextLoads() {
