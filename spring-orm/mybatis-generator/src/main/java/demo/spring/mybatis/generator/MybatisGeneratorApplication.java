8addc4bc89efd0265abcb5d0ab07808d2dd1a2ed
谢军 <sgvenday@outlook.com>
2019-08-27 23:42:24 +0800

    mybatis-generator-demo

---
+++ spring-orm/mybatis-generator/src/main/java/demo/spring/mybatis/generator/MybatisGeneratorApplication.java
@@ -0,0 +1,93 @@
+package demo.spring.mybatis.generator;
+
+import demo.spring.mybatis.generator.mapper.CoffeeMapper;
+import demo.spring.mybatis.generator.model.Coffee;
+import demo.spring.mybatis.generator.model.CoffeeExample;
+import lombok.extern.slf4j.Slf4j;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@SpringBootApplication
+@Slf4j
+@MapperScan("demo.spring.mybatis.generator.mapper")
+public class MybatisGeneratorApplication implements ApplicationRunner {
+	@Autowired
+	private CoffeeMapper coffeeMapper;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisGeneratorApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+//		generateArtifacts();
+//		generateDemo();
+		playWithArtifacts();
+	}
+
+	private void generateArtifacts() throws Exception {
+		List<String> warnings = new ArrayList<>();
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(
+				this.getClass().getResourceAsStream("/generatorConfig.xml"));
+		DefaultShellCallback callback = new DefaultShellCallback(true);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+
+	/**
+	 * 官方示例：http://www.mybatis.org/generator/running/runningWithJava.html
+	 * @throws Exception
+	 */
+	private void generateDemo() throws Exception {
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		String filePath = this.getClass().getResource("/generatorConfig.xml").getPath();
+		File configFile = new File(filePath);
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+		myBatisGenerator.generate(null);
+	}
+
+	private void playWithArtifacts() {
+		Coffee espresso = new Coffee()
+				.withName("espresso")
+				.withPrice(Money.of(CurrencyUnit.of("CNY"), 20.0))
+				.withCreateTime(new Date())
+				.withUpdateTime(new Date());
+		coffeeMapper.insert(espresso);
+
+		Coffee latte = new Coffee()
+				.withName("latte")
+				.withPrice(Money.of(CurrencyUnit.of("CNY"), 30.0))
+				.withCreateTime(new Date())
+				.withUpdateTime(new Date());
+		coffeeMapper.insert(latte);
+
+		Coffee s = coffeeMapper.selectByPrimaryKey(1L);
+		log.info("Coffee {}", s);
+
+		CoffeeExample example = new CoffeeExample();
+		example.createCriteria().andNameEqualTo("latte");
+		List<Coffee> list = coffeeMapper.selectByExample(example);
+		list.forEach(e -> log.info("selectByExample: {}", e));
+	}
+}
+
