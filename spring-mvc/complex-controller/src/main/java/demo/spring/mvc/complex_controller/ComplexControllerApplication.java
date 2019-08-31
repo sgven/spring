8deee364f66627d946fdccc59b48fb3f2f34235d
谢军 <sgvenday@outlook.com>
2019-08-31 19:17:24 +0800

    complex-controller

---
+++ spring-mvc/complex-controller/src/main/java/demo/spring/mvc/complex_controller/ComplexControllerApplication.java
@@ -0,0 +1,17 @@
+package demo.spring.mvc.complex_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableCaching
+public class ComplexControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexControllerApplication.class, args);
+	}
+
+}
