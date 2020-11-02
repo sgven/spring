d3861c4e07edb32de1b11a6e8100b36ab72a55c9
xiejun <xj930312>
2020-11-02 13:54:11 +0800

    data.sql初始化

--- spring-jdbc/datasource-orm-test/README.md
+++ spring-jdbc/datasource-orm-test/README.md
@@ -4,4 +4,5 @@
 
 加入spring-boot-starter-data-jpa依赖后，没有执行data.sql初始化
 
+这时，可以自定义DataSourceInitializer，在spring-boot启动时，执行指定的sql脚本。
 
---
+++ spring-jdbc/datasource-orm-test/src/main/java/demo/spring/datasource/orm/CustomizeDataSourceInitializer.java
@@ -0,0 +1,34 @@
+package demo.spring.datasource.orm;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class CustomizeDataSourceInitializer {
+
+    @Value("classpath:data.sql")
+    private Resource functionScript;
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        // 设置数据源
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScripts(functionScript);
+//        populator.setSeparator("$$");//使用存储过程时，需要配置这个分隔符
+        return populator;
+    }
+}
